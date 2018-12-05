88f1f0f21ee253d9b72253be351107123c7f9ead
Julien Pilleux <julien.pilleux@etu.u-bordeaux.fr>
2018-12-05 15:52:42 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import ctypes
+lib = ctypes.cdll.LoadLibrary('./libhgt2.so')
+
+if __name__ == "__main__":
+    s = "dems/N00E006.hgt"
+    b_string1 = s.encode('utf-8')
+    lib.readDem.argtypes = [ctypes.c_char_p]
+    print(lib.readDem(b_string1))
