3e1b54ee4e1767cb3b35f0e34d4d9b64b71487c2
Guillaume CHUPIN <guillaume.chup@gmail.com>
2018-12-20 15:21:40 +0100

    Leaflet montre une image locale

--- src/js/leaflet.js
+++ src/js/leaflet.js
@@ -1,6 +1,6 @@
 const mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+L.tileLayer('../../dems/618.png', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -29,7 +29,7 @@ const popup = L.popup();
 function onMapClick(e) {
     popup
         .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
+        .setContent("Puta madre")
         .openOn(mymap);
 }
 mymap.on('click', onMapClick);
