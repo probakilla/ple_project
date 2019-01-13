848ddacc2da8476c3db1e5f9480e4983c9021b7c
gchupin <guillaume.chupin@etu.u-bordeaux.fr>
2019-01-13 10:36:43 +0100

    Mineur changement

--- src/js/leaflet.js
+++ src/js/leaflet.js
@@ -1,6 +1,6 @@
 const mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-L.tileLayer('../../dems/618.png', {
+L.tileLayer('../../images/limonde.png', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -10,19 +10,6 @@ L.tileLayer('../../dems/618.png', {
 
 L.marker([51.5, -0.09]).addTo(mymap)
     .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
-
-L.circle([51.508, -0.11], 500, {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5
-}).addTo(mymap).bindPopup("I am a circle.");
-
-L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-]).addTo(mymap).bindPopup("I am a polygon.");
-
 
 const popup = L.popup();
 
