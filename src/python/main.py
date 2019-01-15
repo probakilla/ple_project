1269e34d8bce6b044f665c8f0e689337d012af90
Guillaume CHUPIN <guillaume.chup@gmail.com>
2019-01-15 17:11:06 +0100

    Correction du chemin du fichier

--- src/python/main.py
+++ src/python/main.py
@@ -2,7 +2,7 @@ import ctypes
 lib = ctypes.cdll.LoadLibrary('./libhgt2.so')
 
 if __name__ == "__main__":
-    s = "../../dems/N00E006.hgt"
+    s = "./dems/N00E006.hgt"
     b_string1 = s.encode('utf-8')
     lib.readDem.argtypes = [ctypes.c_char_p]
     print(lib.readDem(b_string1))
