e580f53c5bd73b3ed56f1295f73124f19674c8f7
Julien Pilleux <julien.pilleux@etu.u-bordeaux.fr>
2019-01-21 14:48:14 +0100

    Ajout d'une classe de gestion des requêtes HBase

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,44 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
 const app = express();
+const asyncHandler = require('./asyncHandler');
 
-app.use(express.static(path.join(__dirname, "public")));
+function checkPort(port, portName) {
+    if (isNaN(port) || port < 1024 || port > 65536) {
+	throw new Error('ERROR ! Invalid' + portName +
+		' option value : must be a number between 1024 and 65536');
+    }
+}
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello");
+let hbasePort = '8080';
+let apiPort = '4040';
+opt = require('node-getopt').create([
+    ['p', 'port=ARG', 'Port for the API'],
+    ['h', 'hbase=ARG', 'Port for the hbase rest API']
+]).bindHelp().parseSystem();
+
+if (opt.options.port !== undefined) {
+    checkPort(opt.options.port);
+    apiPort = opt.options.port;
+}
+if (opt.options.hbase !== undefined) {
+    checkPort(opt.options.hbase);
+    hbasePort = opt.options.hbase;
+}
+
+const HBase = require('./hbaseConnection');
+const hbase = new HBase(hbasePort);
+
+app.get('/img/:row&:col', asyncHandler(async (req, res, next) => {
+    const row = req.params.row;
+    const col = req.params.col;
+    const ret = await hbase.getImage(row, col);
+    res.status(200).send(ret);
+}));
+
+app.listen(apiPort, () => {
+    console.log('====== API LINSTENING ======' + '\n' +
+		'API listening on port : ' + apiPort + '\n' +
+		'HBase requests on port : ' + hbasePort);
 });
 
 
-app.listen(4040, () => {
-  console.log("Listening on port 4040...")
-});
--- src/js/hbaseConnection.js
+++ src/js/hbaseConnection.js
@@ -1,17 +1,30 @@
 const fetch = require('node-fetch');
 
-const HEADERS = 'Accept: text/xml';
-const URL = 'http://young:9090/';
+const HEADERS = {'Accept': 'application/json'};
+const BASE_URL = 'http://young:';
 const SCHEMA = '/schema';
+const TABLE = 'pipin'
 
-async function getTableSchema (table) {
-	const tables = URL + table + SCHEMA;
-	return fetch(tables, {
-				method: 'GET',
-				headers: {HEADERS}
-			}).then(res => {
-				return res.text();
-			}).catch(err => {
-				return err.message;
-			});
+class HBaseRequests {
+    constructor(port) {
+	this.port = port;
+    }
+
+    async getImage(row, column) {
+	let url = BASE_URL + this.port + '/' + TABLE + '/' + row + '/' + column;
+	let ret = await fetch(url, {
+	    method: 'GET',
+	    headers: HEADERS
+	}).then(data => {
+	    return data.json();
+	}).then(response => {
+	    return response;
+	}).catch(err => {
+	    console.error(err.message);
+	})
+	console.log(JSON.stringify(ret));
+	return ret;
+    }
 }
+
+module.exports = HBaseRequests;
