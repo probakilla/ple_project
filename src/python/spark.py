b521f329599650321196316607ec2d0ff897a812
gchupin <guillaume.chupin@etu.u-bordeaux.fr>
2019-01-22 10:45:11 +0100

    Rename of a variable

--- src/python/spark.py
+++ src/python/spark.py
@@ -7,11 +7,11 @@ FILENAME_POS = 37
 def spark ():
     sc = SparkContext(CLUSTER, APP_NAME, pyFiles=[__file__])
     num_executor = int(sc.getConf().get('spark.executor.instances'))
-    binaryFiles_rdd = sc.binaryFiles("../raw_data/dem3/",
+    binary_files_rdd = sc.binaryFiles("../raw_data/dem3/",
         minPartitions=num_executor)
-    print(binaryFiles_rdd.take(1)[0][0][FILENAME_POS:])
+    print(binary_files_rdd.take(1)[0][0][FILENAME_POS:])
     print("Number d'executeur", num_executor)
-    print("Nombre de partition sur le rdd", binaryFiles_rdd.getNumPartitions())
+    print("Nombre de partition sur le rdd", binary_files_rdd.getNumPartitions())
 
 if __name__ == "__main__":
     spark()
