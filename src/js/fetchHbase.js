783de3c2af78b1bb27df451d6612f8a33b1051a1
Julien Pilleux <julien.pilleux@etu.u-bordeaux.fr>
2019-01-23 18:19:41 +0100

    Ajout du chargement d'une image par défaut

--- src/js/fetchHbase.js
+++ src/js/fetchHbase.js
@@ -1,51 +1,76 @@
-require('dotenv').config();
-const URL = 'http://' + process.env.POST_NAME + ':' + process.env.API_PORT + '/img/';
+const URL = 'http://batman:9696/img/';
+const DEFAULT_URI = '690699&N:E';
 const HEADERS = {'Accept': 'application/json'};
 const MAX_LAT = 90;
 const MAX_LONG = 180;
+const PNG_METADATA = 'data:image/png;base64,';
 
 function displayMessage(msg) {
     document.getElementById('uri-disp').innerHTML = msg;
 }
 
-function correctLat(lat) {
-    let ret = '';
-    if (lat < 0) {
-	res += 'S';
-	lat *= -1;
-    } else {
-	res += 'N';
-    }
-    return ret + (lat > MAX_LAT) ? MAX_LAT : lat;
+function displayImage(img) {
+    document.getElementById('img-disp').src = img;
 }
 
-function correctLong(lng) {
-    let ret = '';
-    if (lng < 0) {
-	res += 'W';
-	lat *= -1;
-    } else {
-	res += 'E';
-    }
-    return ret + (lng > MAX_LONG) ? MAX_LONG : lng;
-}
-
-function buildRequestURI(lat, lng, zoom) {
-    let uri = correctLat(lat) + correctLong(lng);
-    return uri + zoom;
-}
-
-async function fetchImage(lat, lng, zoom) {
-    const url = URL + buildRequestURI(lat, lng, zoom);
-    displayMessage('Fetching url : ' + url);
+async function getDefaultImage() {
+    const url = URL + DEFAULT_URI; 
     return await fetch(url, {
 	method: 'GET',
 	headers: HEADERS
     }).then(data => {
 	return data.json();
     }).then(response => {
-	return response.Row[0].Cell[0].$;
+	return PNG_METADATA + response.Row[0].Cell[0].$;
     }).catch(err => {
-	console.error(err.message);
+	return err.message;
+    })
+}
+
+/**
+ * Return the URI of the wanted file in HBase
+ * @param {string} lat The latitude
+ * @param {string} lng The longitude
+ * @param {string} zoom The zoom level
+ * @returns {string} Retrieves a correct URI for the hbase request e.g. : N43W0455
+ */
+function buildRequestURI(lat, lng, zoom) {
+    lat = Number(lat);
+    lng = Number(lng);
+    let card = '';
+    let uri = '';
+    
+    if (lat < 0) {
+	lat *= -1;
+	card += 'S';
+    } else {
+	card += 'N';
+    }
+    uri += (lat > MAX_LAT) ? MAX_LAT.toString() : lat.toString();
+    
+    card += ':';
+    if (lng < 0) {
+	lng *= -1;
+	card += 'W';
+    } else {
+	card += 'E';
+    }
+    uri += (lng > MAX_LONG) ? MAX_LONG.toString() : lng.toString();
+    uri += zoom + '&' + card;
+    return uri
+}
+
+async function fetchImage(lat, lng, zoom) {
+    const url = URL + buildRequestURI(lat, lng, zoom);
+    displayMessage(url);
+    return await fetch(url, {
+	method: 'GET',
+	headers: HEADERS
+    }).then(data => {
+	return data.json();
+    }).then(response => {
+	return PNG_METADATA + response.Row[0].Cell[0].$;
+    }).catch(async (err) => {
+	return await getDefaultImage();
     });
 }
--- src/js/leaflet.js
+++ src/js/leaflet.js
@@ -1,12 +1,11 @@
 const mapboxAttribution = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
-const PNG_METADATA = 'data:image/png;base64;';
 
 L.TileLayer.MyCustomLayer = L.TileLayer.extend({
     getTileUrl: async (coords) => {
-	let img = PNG_METADATA + await fetchImage(coords.y, coords.y, coords.z);
-	console.log(img);
+	let img = await fetchImage(coords.y, coords.y, coords.z);
+	displayImage(img);
 	return img;
     }
 });
@@ -26,38 +25,7 @@ function onLoad() {
     let mymap = L.map('mapid').setView([51.505, -0.09], 9);
     L.tileLayer.myCustomLayer('{x}{y}{z}', {
 	minZoom: 0,
-	maxZoom: 9
+	maxZoom: 9,
+	attribution: mapboxAttribution
     }).addTo(mymap);
 }
-
-(async() => {
-    // TMP PENDANT QUE HBASE EST CASSE
-    return null;
-    // TMP PENDANT QUE HBASE EST CASSE
-    
-    const img = 'data:image/png;base64,' + fetchImage('69', '-069', '9');
-    const greyscale = L.tileLayer('../../images/limonde.png', {
-	maxZoom: 18,
-	attribution: mapboxAttribution,
-	id: 'MapID'
-    });
-
-    const streets = L.tileLayer(img, {
-	id: 'MapID',
-	attibution: mapboxAttribution,
-	maxZoom: 18
-    });
-
-    let mymap = L.map('mapid', {
-	center: [51.505, -0.09],
-	zoom: 13,
-	layers: [greyscale, streets]
-    });
-
-    mymap.on('click', onMapClick);
-
-    L.marker([51.5, -0.09]).addTo(mymap)
-	.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
-
-    const popup = L.popup();
-})();
