112da871b065416640022ecab2e82247a7326a2f
Julien Pilleux <julien.pilleux@etu.u-bordeaux.fr>
2019-01-24 17:15:15 +0100

    changement des routes du serveur

--- src/server/hbaseConnection.js
+++ src/server/hbaseConnection.js
@@ -1,27 +1,30 @@
-require('dotenv').config();
-const fetch = require('node-fetch');
-const HEADERS = {'Accept': 'application/json'};
+require("dotenv").config();
+const fetch = require("node-fetch");
+const HEADERS = { Accept: "application/json" };
 const TABLE = process.env.HBASE_TABLE;
+const PNG_METADATA = "data:image/png;base64,";
 
 class HBaseRequests {
-    constructor(postName, port) {
-	this.url = 'http://localhost:' + port;
-    }
+  constructor(postName, port) {
+    this.url = "http://localhost:" + port;
+  }
 
-    async getImage(row, column) {
-	let url = this.url + '/' + TABLE + '/' + row + '/' + column;
-	let ret = await fetch(url, {
-	    method: 'GET',
-	    headers: HEADERS
-	}).then(data => {
-	    return data.json();
-	}).then(response => {
-	    return response;
-	}).catch(err => {
-	    console.error(err.message);
-	})
-	return ret;
-    }
+  async getImage(row, column) {
+    let url = this.url + "/" + TABLE + "/" + row + "/" + column;
+    return await fetch(url, {
+      method: "GET",
+      headers: HEADERS
+    })
+      .then(data => {
+        return data.json();
+      })
+      .then(res => {
+        return PNG_METADATA + res.Row[0].Cell[0].$;
+      })
+      .catch(err => {
+        console.error(err.message);
+      });
+  }
 }
 
 module.exports = HBaseRequests;
