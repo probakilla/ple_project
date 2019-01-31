bf1e88f7fbd9c34562055ff209f1eb2d20d2e9e5
Julien Pilleux <julien.pilleux@etu.u-bordeaux.fr>
2019-01-31 09:58:16 +0100

    Amélioration du mode zidane

--- src/js/interface.js
+++ src/js/interface.js
@@ -4,12 +4,17 @@ function devenirRiche() {
 
 function zidaneMode() {
   document.body.style.backgroundImage = "url('../../images/zidane_1.jpg')";
-  const jumbo = document.getElementById("jumbo")
+  const jumbo = document.getElementById("jumbo");
   jumbo.style.backgroundImage = "url('../../images/zidane.jpg')";
-  const footer = document.getElementById("footer")
-  footer.style.backgroundImage = "url('../../images/zidane_2.jpeg')"
-  const card = document.getElementById("card")
-  card.style.backgroundImage = "url('../../images/zidane_3.jpeg')"
+  const footer = document.getElementById("footer");
+  footer.style.backgroundImage = "url('../../images/zidane_2.jpeg')";
+  const card = document.getElementById("card");
+  card.style.backgroundImage = "url('../../images/zidane_3.jpeg')";
+  const btn = document.getElementById("btn");
+  btn.classList.add("btn-zidane");
+  const leaflet = document
+    .getElementById("map")
+    .classList.add("leaflet-container-zidane");
 }
 
 class Display {
